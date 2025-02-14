993da33771b46e40ece9ead4ad43678c8ab7957d
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-02-14 19:09:55 +0200

    get task that only created by user

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\Builder;
 
 class Task extends Model
 {
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+
+    protected static function booted(): void
+    {
+        self::addGlobalScope(function (Builder $query) {
+            if (auth()->check() && !auth()->user()->is_admin) {
+                $query->where('user_id', auth()->id());
+            }
+        });
     }
 }
