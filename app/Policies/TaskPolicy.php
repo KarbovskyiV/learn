0677869740c594c946ec70da43c78b4d4d3967e7
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-02-15 12:54:12 +0200

    refactor: update readability by using constants

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Console\View\Components\Task;
 
@@ -9,17 +10,17 @@ class TaskPolicy
 {
     public function create(User $user): bool
     {
-        return $user->role_id == 2;
+        return $user->role_id == Role::ROLE_ADMIN;
     }
 
     public function update(User $user, Task $task): bool
     {
-        return in_array($user->role_id, [2, 3])
+        return in_array($user->role_id, [Role::ROLE_ADMIN, Role::ROLE_MANAGER])
             || $task->user_id === $user->id;
     }
 
     public function delete(User $user, Task $task): bool
     {
-        return $user->role_id == 2;
+        return $user->role_id == Role::ROLE_ADMIN;
     }
 }
